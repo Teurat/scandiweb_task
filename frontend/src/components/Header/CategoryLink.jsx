ac58f0f0504214d246b27a10de20eaecfd7a843e
Teurat Murtezai <teuratmurtezai@gmail.com>
2025-09-06 15:49:15 +0200

    Fixed header styling

--- frontend/src/components/Header/CategoryLink.jsx
+++ frontend/src/components/Header/CategoryLink.jsx
@@ -5,17 +5,18 @@ export default function CategoryLink({ name }) {
   const isActive = useLocation().pathname === path;
 
   return (
-    <Link
-      to={path}
-      data-testid={isActive ? 'active-category-link' : 'category-link'}
-      className={`relative uppercase text-sm tracking-wider pb-6 transition-colors ${
-        isActive ? 'text-green-300' : 'text-gray-700 hover:text-brand'
-      }`}
-    >
-      {name}
-      {isActive && (
-        <span className="absolute left-0 -bottom-0.5 w-full h-[2px] bg-brand" />
-      )}
-    </Link>
+<Link
+  to={path}
+  data-testid={isActive ? 'active-category-link' : 'category-link'}
+  className={`relative uppercase text-sm tracking-wider h-full flex items-center px-2 transition-colors ${
+    isActive ? 'text-green-500' : 'text-gray-700 hover:text-brand'
+  }`}
+>
+  {name}
+  {isActive && (
+    <span className="absolute left-0 bottom-0 w-full h-[2px] bg-green-500" />
+  )}
+</Link>
+
   );
 }
